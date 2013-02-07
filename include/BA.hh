c6e56d67ace1b83b5d21f1898a75f6f2a6862042
William Ellison <wae@rt38.net>
2013-02-07 16:40:08 -0500

    Added the basic header file for the project.

---
+++ include/BA.hh
@@ -0,0 +1,15 @@
+#include <BA/Platform.hh>
+#include <BA/Common.hh>
+
+#if ! defined _BA_HH
+#define _BA_HH
+using namespace std;
+
+#if ! defined __cplusplus
+#error "C++ supported only."
+#endif
+
+/* Procedural functions we'll need to talk to the standard bits */
+void error(string *, ...);
+
+#endif /* defined _BA_HH */
