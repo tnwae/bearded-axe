f7866abf832d664064e4638263fe1efdde89bde0
William Ellison <wae@rt38.net>
2013-02-07 18:52:35 -0500

    Added header file for Scene class.
    Scenes contain the global rendering context ("GlobalState") and the objects
    to be rendered.

---
+++ include/BA/Scene.hh
@@ -0,0 +1,26 @@
+#if ! defined _BA_SCENE_HH
+#define _BA_SCENE_HH
+
+#include <deque>
+#include <vector>
+#include <BObject.hh>
+#include <BA/GlobalState.hh>
+#include <BA/RenderObject.hh>
+using namespace std;
+
+namespace BA {
+class Scene : public BObject {
+public:
+   Scene(GlobalState *);
+   ~Scene();
+   void render();
+   GlobalState *getGlobalStateById(uint);
+protected:
+   Scene();
+private:
+   deque<RenderObject *> objects;
+   vector<GlobalState *> globalStates;
+};
+}
+
+#endif
