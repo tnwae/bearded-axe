214217fbe12ae84c7400e9a07af3b9085b45bb20
William Ellison <wae@rt38.net>
2013-02-07 18:53:15 -0500

    Added a global state object.
    GlobalStates include information needed by the renderer.

---
+++ include/BA/GlobalState.hh
@@ -0,0 +1,23 @@
+
+#ifndef _BA_GLOBALSTATE_HH
+#define _BA_GLOBALSTATE_HH
+#include <vector>
+using namespace std;
+
+#include <BObject.hh>
+#include <BA/Vector3.hh>
+
+namespace BA {
+   const int GS_TEXTURE  = 0;
+   const int GS_SHADOW   = 1;
+   const int GS_SPECULAR = 7;
+
+   class GlobalState : public BObject {
+public:
+
+private:
+
+};
+}
+
+#endif
