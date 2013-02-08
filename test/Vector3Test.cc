89eb527b8933cf72775d9ca05bf07604de5d34e7
William Ellison <wae@rt38.net>
2013-02-07 21:19:47 -0500

    Added a makefile to the test suite and a new test to v3test
    Added an output format verification to Vector3Test.

--- test/Vector3Test.cc
+++ test/Vector3Test.cc
@@ -1,4 +1,4 @@
-#include "Vector3.hh"
+#include <BA/Vector3.hh>
 using namespace BA;
 
 #include <iostream>
@@ -19,6 +19,8 @@ int main(int argc, char **argv) {
    Vector3<float> vh = va - va;
    Vector3<float> vi = vb.normalize();
 
+// KNOWN VALUES.
+// I found these either by hand or with the help of Wolfram Alpha.
    float dot = 10;
    float norm = 40;
    Vector3<float> scale(2, 4, 6);
@@ -26,18 +28,35 @@ int main(int argc, char **argv) {
    Vector3<float> sum(4, 4, 4);
    Vector3<float> diff(0, 0, 0);
 
+// test 1: dot product
    assert(ve == dot);
    cout << "test 1 passed (dot product)" << endl;
+
+// test 2: sum
    assert(vc == sum);
    cout << "test 2 passed (sum)" << endl;
+
+// test 3: norm
    assert(vg == norm);
    cout << "test 3 passed (norm)" << endl;
+
+// test 4: cross product
    assert(cprod == vf);
    cout << "test 4 passed (cross product)" << endl;
+
+// test 5: scale vector by single number amount
    assert(scale == vd);
    cout << "test 5 passed (scalar)" << endl;
+
+// test 6: difference
    assert(diff == vh);
    cout << "test 6 passed (difference)" << endl;
-   assert(fabs(vi.norm())-vi.norm() < FLT_EPSILON);
+
+// norm of a normalized vector = 1
+   assert(fabs(vi.norm()) - 1 < FLT_EPSILON);
    cout << "test 7 passed (norm of normalized = 1)" << endl;
+
+// print format correct
+   assert(vh.toString().compare("(0, 0, 0)") == 0);
+   cout << "test 8 passed (print format correct)" << endl;
 }
