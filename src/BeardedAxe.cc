73e346dcc6fb18a4de587b31016481d64222dbe5
William Ellison <wae@rt38.net>
2013-02-08 17:29:23 -0500

    Changed gluLookAt parameter to get a point-on view of the cube.

--- src/BeardedAxe.cc
+++ src/BeardedAxe.cc
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(0., 8., 8.,
+  gluLookAt(8., 8., 8.,
             0., 0., 0.,
             0., 1., 0.);
 
