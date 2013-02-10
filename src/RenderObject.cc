c81e95664045454f278c68af25dbe576fe078cfc
William Ellison <wae@rt38.net>
2013-02-09 19:37:18 -0500

    Implemented a few methods of RenderObject.
    Implemented the constructor, destructor, `getVertexCount`, and
    `toString` methods of RenderObject.

---
+++ src/RenderObject.cc
@@ -0,0 +1,54 @@
+#include <BA/RenderObject.hh>
+#include <BA/Vector3.hh>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+namespace BA {
+   RenderObject::RenderObject(std::string _name, std::string _filename, RenderType _renderType) {
+      name = _name;
+      renderType = _renderType;
+
+      vertices = new std::vector<Vector3<float> *>;
+      facets = new std::vector<Vector3<float> *>;
+      surfaceNormals = new std::vector<Vector3<float> *>;
+      vertexNormals = new std::vector<Vector3<float> *>;
+
+   }
+
+   RenderObject::~RenderObject() {
+      std::vector<Vector3<float>*>::iterator vit;
+
+      for(vit = vertices->begin(); vit != vertices->end(); vit++) {
+	 delete *vit;
+      }
+
+      delete vertices;
+
+      for(vit = facets->begin(); vit != facets->end(); vit++) {
+	 delete *vit;
+      }
+
+      delete facets;
+
+      for(vit = surfaceNormals->begin(); vit != surfaceNormals->end(); vit++) {
+	 delete *vit;
+      }
+
+      delete surfaceNormals;
+
+      for(vit = vertexNormals->begin(); vit != vertexNormals->end(); vit++) {
+	 delete *vit;
+      }
+
+      delete vertexNormals;
+   }
+
+   int RenderObject::getVertexCount() {
+      return vertices->size();
+   }
+
+   std::string RenderObject::toString() {
+      return name;
+   }
+}
