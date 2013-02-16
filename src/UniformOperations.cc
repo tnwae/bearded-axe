edfdcfee057502e751c4339046724ab0915be3fa
William Ellison <wae@rt38.net>
2013-02-16 16:26:37 -0500

    Added Vector3 support to UniformOperations

--- src/UniformOperations.cc
+++ src/UniformOperations.cc
@@ -1,14 +1,5 @@
-#if defined __APPLE__
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
-#include <OpenGL/glext.h>
-#include <GLUT/glut.h>
-#else
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glext.h>
-#include <GL/glut.h>
-#endif
+#include <BA.hh>
+#include <BA/Vector3.hh>
 #include <BA/Uniforms.hh>
 #include <vector>
 #include <iostream>
@@ -82,6 +73,34 @@ namespace BA {
     return uniform_location;  
   }
 
+  int setUniform(unsigned shad, std::string which, unsigned value)
+  {
+    GLint uniform_location = getUniformLocation(shad, which);
+    if(uniform_location != -1) glUniform1ui(uniform_location, value);
+    return uniform_location;  
+  }
+
+  int setUniform(unsigned shad, std::string which, unsigned x, unsigned y)
+  {
+    GLint uniform_location = getUniformLocation(shad, which);
+    if(uniform_location != -1) glUniform2ui(uniform_location, x, y);
+    return uniform_location;  
+  }
+
+  int setUniform(unsigned shad, std::string which, unsigned x, unsigned y, unsigned z)
+  {
+    GLint uniform_location = getUniformLocation(shad, which);
+    if(uniform_location != -1) glUniform3ui(uniform_location, x, y, z);
+    return uniform_location;  
+  }
+
+  int setUniform(unsigned shad, std::string which, unsigned x, unsigned y, unsigned z, unsigned w) 
+  {
+    GLint uniform_location = getUniformLocation(shad, which);
+    if(uniform_location != -1) glUniform4ui(uniform_location, x, y, z, w);
+    return uniform_location;  
+  }
+
   int setUniform(unsigned shad, std::string which, std::vector<float> array)
   {
     switch(array.size()) {
@@ -104,4 +123,26 @@ namespace BA {
     }
   }
 
+  int setUniform(unsigned shad, std::string which, std::vector<unsigned> array)
+  {
+    switch(array.size()) {
+      case 1: return setUniform(shad, which, array[0]);
+      case 2: return setUniform(shad, which, array[0], array[1]);
+      case 3: return setUniform(shad, which, array[0], array[1], array[2]);
+      case 4: return setUniform(shad, which, array[0], array[1], array[2], array[3]);
+      default: return -1;
+    }
+  }
+
+  int setUniform(unsigned shad, std::string which, Vector3<int> value) {
+    return setUniform(shad, which, value.x, value.y, value.z);
+  }
+
+  int setUniform(unsigned shad, std::string which, Vector3<float> value) {
+    return setUniform(shad, which, value.x, value.y, value.z);
+  }
+
+  int setUniform(unsigned shad, std::string which, Vector3<unsigned> value) {
+    return setUniform(shad, which, value.x, value.y, value.z);
+  }
 }
