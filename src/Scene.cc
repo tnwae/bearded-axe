5783be080a05c64fe8de1648fd3216a1e0ef122e
William Ellison <wae@rt38.net>
2013-08-13 00:44:46 -0400

    Added downcast tests for cube and sphere prims

--- src/Scene.cc
+++ src/Scene.cc
@@ -2,31 +2,36 @@
 #include <BA/GlobalState.hh>
 #include <BA/RenderObject.hh>
 #include <BA/TeapotObject.hh>
+#include <BA/SphereObject.hh>
 #include <BA/Vector3.hh>
 #include <iostream>
 #include <utility>
 #include <vector>
 
 namespace BA {
-    Scene::Scene(GlobalState *gs) 
-    {
-        globalStates.push_back(gs);
-    }
+  Scene::Scene(GlobalState *gs)
+  {
+    globalStates.push_back(gs);
+  }
 
-    void Scene::addObject(RenderObject *ro) 
-    {
-        objects.push_back(ro);
-    }
+  void Scene::addObject(RenderObject *ro)
+  {
+    objects.push_back(ro);
+  }
 
-    void Scene::render() 
-    {
-        for(RenderObject *ro : objects) {
-            if(TeapotObject *tpt = dynamic_cast<TeapotObject *>(ro)) {
-                    tpt->render();
-            } else {
-                ro->render();
-            }
-        }
-    }    
+  void Scene::render()
+  {
+    for(RenderObject *ro : objects) {
+      if(TeapotObject *tpt = dynamic_cast<TeapotObject *>(ro)) {
+        tpt->render();
+      } else if(SphereObject *s = dynamic_cast<SphereObject *>(ro)) {
+        s->render();
+      } else if(CubeObject *s = dynamic_cast<CubeObject *>(ro)) {
+        s->render();
+      } else {
+        ro->render();
+      }
+    }
+  }
 }
 
